d987ec0b0bedb7d057d0656ad080acd665904077
ALBERT44-T <71137504+ALBERT44-T@users.noreply.github.com>
2022-01-23 22:46:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -39,8 +39,8 @@
 	 </style>
 </head>
 <body>
-<h1 >Translation App</h1>
-<div>
+<h1 style="color:white;">Translation App</h1>
+<!-- <div> -->
 <?php
         $word = filter_input(INPUT_POST,'word');
         $kinyarwanda = filter_input(INPUT_POST, 'kinya');
@@ -73,14 +73,13 @@ mysqli_close($conn); // Close connection
 }
 ?>
 
-</div>
+<!-- </div>
  <br/>
  <br/>
- <br/>
-	<div>
+ <br/> -->
+	<!-- <div> -->
 		
      <form style="margin-top:30px; margin-left:17px;" action="" method="post">
-     	<section>
      	<div class="container" id="container">
      		<h1> Register a V_Word</h1>
              <br/>
@@ -105,128 +104,5 @@ mysqli_close($conn); // Close connection
      	</div>
      </form>
  
-     
-     <div class="containers" style="margin-left:670px; width:70%; margin-top:-430px;">
-   <div class="row">
-   <!-- <div class="col-md-8 col-md-offset-2" style="margin-top: 5%;"> -->
-   <div class="row">
-
-   </table>
-	  <form method="POST" style="width:60%; background_color:black; margin-top:-437px"> 
-	   <table>
- 
-       <!-- <table bgcolor="whitesmoke" width="50%;height:70%" cellspacing="20" border="0"> -->
-				<tr ><td colspan="2"><h1>Select Word to Translate</h1></tr>
-                <br/>
-				
-	<tr>			
-	<td> Choose Variable word you want to translate</td>
-	<td><!-- <select name="status" id="status" onchange="sayIt()">
-				<option name="gura">V_Gura</option>
-				<option name="tuma">V_Rangura</option>
-				<option name="rangura">V_Gurisha</option>
-				<option name="Gurisha">V_Tuma</option>
-				
-			</select> -->
-            <select name="word" id="val" style="background: #5cb85c; color: white;">
-    <option value="0" style="background: #5cb85c; color: #fff;">-- Select word --</option>
-    <?php
-        include "conn.php";  // Using database connection file here
-        $records = mysqli_query($db, "SELECT * From indimi");  // Use select query here 
-
-        while($data = mysqli_fetch_array($records))
-        {
-            echo "<option value='". $data['id'] ."'>" .$data['variable'] ."</option>";  // displaying data in option menu
-        }	
-    ?>  
-  </select>
-			</td>
-            <br/>
-            <br/>
-			                  <td style="margin-left:600px">Translate To </td>
-			<td>
-				<select name="status" style="background: #5cb85c; color: white;">
-			    <option value="0 " style="background: #5cb85c; color: white;">-- Select language --</option>
-				<option value="1" style="background: #5cb85c; color: white;">Kinyarwanda</option>
-				<option value="2" style="background: #5cb85c; color: white;">French</option>
-				<option value="3" style="background: #5cb85c; color: white;">English</option>
-				<option value="4" style="background: #5cb85c; color: white;">Swahili</option>
-				
-			</select></td>
-			<td>
-                <br/>
-                <br/>
-                  <button name="translate" id="translate" class="btn" style="color: white;border-color: white;background-color: light; background-color:#4CAF50;height:30px;">Translate</button>
-                   </td>
-     </tr>
-     <tr>
-		 <?php 
-		 $result=[];
-		 if(isset($_POST['translate']))
-		 {	
-			$result[0] = " ";
-			 $language = $_POST['status'];
-			 $word = $_POST['word'];
-			 if(($language == 0) || ($word == 0))
-			 {
-				 $result[0] = "choose valid data";
-
-			 }
-			 else {
-				 
-				$query_select_indimi= mysqli_query($db, "SELECT * FROM indimi where id='$word'");
-				if(mysqli_num_rows($query_select_indimi) > 0)
-				{
-					$result[0] = "one element found";
-
-					$data_re = $query_select_indimi->fetch_array();
-
-			if($language == 1)
-			{
-				$result[0] = $data_re['kinyarwanda'];
-			}
-			else if ($language == 2)
-			{
-				$result[0] = $data_re['french'];
-			}	else if ($language == 3)
-			{
-				$result[0] = $data_re['english'];
-			}	else if ($language == 4)
-			{
-				$result[0] = $data_re['swahili'];
-			}
-			else{
-				$result[0]="couldn't find";
-			}
-				}
-				else{
-					$result[0] = "no word found";
-				}
-
-			 }
-
-			
-	
-				
-			
-
-		
-?> 
-<br/>
-<br/>
-  <td>Your Translated Result<br/><br/></td><td><label></label><b style="font-size:40px;"><?php echo $result[0];?></td><?php
-		 }
-
-		 	 ?>
-   
-        </b>
-    
-     </tr>
-
-          
-                   
-</table>
-		</form>     
-
 </body>
 </html>
